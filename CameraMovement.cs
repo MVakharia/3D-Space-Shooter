7e3676a9782ad968a63ecd12fd6d1b21eb204a8a
MVakharia <michaelvakharia@gmail.com>
2021-04-16 13:25:13 +0100

    Started development.

---
+++ CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject target;
+
+    [SerializeField]
+    private float moveSpeed;
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
+
+        transform.rotation = target.transform.rotation;
+    }
+}
---
+++ PlayerSpaceshipMovement.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+
+public class PlayerSpaceshipMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float currentMoveSpeed;
+
+    [SerializeField]
+    private float maxMoveSpeed;
+
+    [SerializeField]
+    private float verticalMovementAxis;
+
+    [SerializeField]
+    private float turnSpeed;
+
+    private float VerticalMovementAxis
+    {
+        get
+        {
+            if(verticalMovementAxis > 1)
+            {
+                verticalMovementAxis = 1;
+            }
+            if(verticalMovementAxis < -1)
+            {
+                verticalMovementAxis = -1;
+            }
+            return verticalMovementAxis;
+        }
+        set
+        {
+            verticalMovementAxis = value;
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            if (Cursor.lockState == CursorLockMode.Locked)
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+
+            if(Cursor.lockState == CursorLockMode.None)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+
+
+        YMovementInput();
+
+        Move();
+
+        if(Input.GetAxisRaw("Mouse X") > 0)
+        {
+            transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetAxisRaw("Mouse X") < 0)
+        {
+            transform.Rotate(0, -turnSpeed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetAxisRaw("Mouse Y") > 0)
+        {
+            transform.Rotate(turnSpeed * Time.deltaTime, 0, 0);
+        } 
+
+        if(Input.GetAxisRaw("Mouse Y") < 0)
+        {
+            transform.Rotate(-turnSpeed * Time.deltaTime, 0, 0);
+        }
+    }
+
+    private void YMovementInput ()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            VerticalMovementAxis += 1;
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            VerticalMovementAxis -= 1;
+        }
+        else if(!Input.GetKey(KeyCode.Space) && !Input.GetKey(KeyCode.LeftControl))
+        {
+            VerticalMovementAxis = 0;
+        }
+    }
+
+    private void Move()
+    {
+        transform.Translate(MovementVector() * Time.deltaTime);
+    }
+
+    private Vector3 MovementVector ()
+    {
+        return new Vector3(Input.GetAxisRaw("Horizontal"), VerticalMovementAxis, Input.GetAxisRaw("Vertical")) * currentMoveSpeed;
+    }
+}
