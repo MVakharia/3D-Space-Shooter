d785b79365a7d005dc0fe9a595428156d874dbbb
MVakharia <michaelvakharia@gmail.com>
2021-04-17 20:25:50 +0100

    Update PlayerSpaceshipMovement.cs

--- PlayerSpaceshipMovement.cs
+++ PlayerSpaceshipMovement.cs
@@ -2,27 +2,87 @@
 
 public class PlayerSpaceshipMovement : MonoBehaviour
 {
-    [SerializeField]
-    private float currentMoveSpeed;
+    [SerializeField] private float currentMoveSpeed;
+    [SerializeField] private float maxMoveSpeed;
+    [SerializeField] private float verticalMovementAxis;
+    [SerializeField] private float turnSpeed;
+    [SerializeField] private float acceleration;
+    [SerializeField] private float deceleration;
 
-    [SerializeField]
-    private float maxMoveSpeed;
+    [SerializeField] private float xMoveSpeed;
+    private float XMoveSpeed
+    {
+        get
+        {
+            if (xMoveSpeed > maxMoveSpeed)
+            {
+                xMoveSpeed = maxMoveSpeed;
+            }
+            if (xMoveSpeed < -maxMoveSpeed)
+            {
+                xMoveSpeed = -maxMoveSpeed;
+            }
+            return xMoveSpeed;
+        }
+        set
+        {
+            xMoveSpeed = value;
+        }
+    }
 
-    [SerializeField]
-    private float verticalMovementAxis;
+    [SerializeField] private float yMoveSpeed;
 
-    [SerializeField]
-    private float turnSpeed;
+    private float YMoveSpeed
+    {
+        get
+        {
+            if (yMoveSpeed > maxMoveSpeed)
+            {
+                yMoveSpeed = maxMoveSpeed;
+            }
+            if (yMoveSpeed < -maxMoveSpeed)
+            {
+                yMoveSpeed = -maxMoveSpeed;
+            }
+            return yMoveSpeed;
+        }
+        set
+        {
+            yMoveSpeed = value;
+        }
+    }
+
+    [SerializeField] private float zMoveSpeed;
+
+    private float ZMoveSpeed
+    {
+        get
+        {
+            if (zMoveSpeed > maxMoveSpeed)
+            {
+                zMoveSpeed = maxMoveSpeed;
+            }
+            if (zMoveSpeed < -maxMoveSpeed)
+            {
+                zMoveSpeed = -maxMoveSpeed;
+            }
+            return zMoveSpeed;
+        }
+        set
+        {
+            zMoveSpeed = value;
+        }
+    }
 
     private float VerticalMovementAxis
     {
         get
         {
-            if(verticalMovementAxis > 1)
+            if (verticalMovementAxis > 1)
             {
                 verticalMovementAxis = 1;
             }
-            if(verticalMovementAxis < -1)
+            if (verticalMovementAxis < -1)
             {
                 verticalMovementAxis = -1;
             }
@@ -34,48 +94,96 @@ public class PlayerSpaceshipMovement : MonoBehaviour
         }
     }
 
-    private void Update()
+    private void ToggleLockCursor ()
     {
-        if(Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(KeyCode.P))
         {
             if (Cursor.lockState == CursorLockMode.Locked)
             {
                 Cursor.lockState = CursorLockMode.None;
             }
 
-            if(Cursor.lockState == CursorLockMode.None)
+            if (Cursor.lockState == CursorLockMode.None)
             {
                 Cursor.lockState = CursorLockMode.Locked;
             }
         }
+    }
 
+    private void MouseLook() => transform.Rotate(turnSpeed * Time.deltaTime * Input.GetAxisRaw("Mouse Y"), turnSpeed * Time.deltaTime * Input.GetAxisRaw("Mouse X"), 0);
+
+    private void Update()
+    {
+        ToggleLockCursor();
 
         YMovementInput();
 
         Move();
 
-        if(Input.GetAxisRaw("Mouse X") > 0)
+        MouseLook();
+
+        if(Input.GetAxisRaw("Horizontal") == 0)
         {
-            transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
+            if(XMoveSpeed > 0)
+            {
+                XMoveSpeed -= deceleration * Time.deltaTime;
+            }
+
+            if(XMoveSpeed < 0)
+            {
+                XMoveSpeed += deceleration * Time.deltaTime;
+            }
+
+            if(XMoveSpeed < 0.1F && XMoveSpeed > -0.1F)
+            {
+                XMoveSpeed = 0;
+            }
         }
 
-        if (Input.GetAxisRaw("Mouse X") < 0)
+        XMoveSpeed += Input.GetAxisRaw("Horizontal") * acceleration * Time.deltaTime;
+
+        if (VerticalMovementAxis == 0)
         {
-            transform.Rotate(0, -turnSpeed * Time.deltaTime, 0);
+            if (YMoveSpeed > 0)
+            {
+                YMoveSpeed -= deceleration * Time.deltaTime;
+            }
+
+            if (YMoveSpeed < 0)
+            {
+                YMoveSpeed += deceleration * Time.deltaTime;
+            }
+
+            if (YMoveSpeed < 0.1F && YMoveSpeed > -0.1F)
+            {
+                YMoveSpeed = 0;
+            }
         }
 
-        if (Input.GetAxisRaw("Mouse Y") > 0)
-        {
-            transform.Rotate(turnSpeed * Time.deltaTime, 0, 0);
-        } 
+        YMoveSpeed += VerticalMovementAxis * acceleration * Time.deltaTime;
 
-        if(Input.GetAxisRaw("Mouse Y") < 0)
+        if (Input.GetAxisRaw("Vertical") == 0)
         {
-            transform.Rotate(-turnSpeed * Time.deltaTime, 0, 0);
+            if (ZMoveSpeed > 0)
+            {
+                ZMoveSpeed -= deceleration * Time.deltaTime;
+            }
+
+            if (ZMoveSpeed < 0)
+            {
+                ZMoveSpeed += deceleration * Time.deltaTime;
+            }
+
+            if (ZMoveSpeed < 0.1F && ZMoveSpeed > -0.1F)
+            {
+                ZMoveSpeed = 0;
+            }
         }
+
+        ZMoveSpeed += Input.GetAxisRaw("Vertical") * acceleration * Time.deltaTime;
     }
 
-    private void YMovementInput ()
+    private void YMovementInput()
     {
         if (Input.GetKey(KeyCode.Space))
         {
@@ -85,7 +193,7 @@ public class PlayerSpaceshipMovement : MonoBehaviour
         {
             VerticalMovementAxis -= 1;
         }
-        else if(!Input.GetKey(KeyCode.Space) && !Input.GetKey(KeyCode.LeftControl))
+        else if (!Input.GetKey(KeyCode.Space) && !Input.GetKey(KeyCode.LeftControl))
         {
             VerticalMovementAxis = 0;
         }
@@ -96,8 +204,10 @@ public class PlayerSpaceshipMovement : MonoBehaviour
         transform.Translate(MovementVector() * Time.deltaTime);
     }
 
-    private Vector3 MovementVector ()
+    private Vector3 MovementVector()
     {
-        return new Vector3(Input.GetAxisRaw("Horizontal"), VerticalMovementAxis, Input.GetAxisRaw("Vertical")) * currentMoveSpeed;
+        return new Vector3(XMoveSpeed, YMoveSpeed, ZMoveSpeed);
     }
+
+
 }
